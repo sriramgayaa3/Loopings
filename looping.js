fbc62d780c626f0b6c34cae559384fd289ddf75f
sriramgayaa3 <2211cs010545@mallareddyuniversity.ac.in>
2025-01-08 14:35:26 +0530

    Loopings

---
+++ looping.js
@@ -0,0 +1,10 @@
+let = 100;
+for(let i=0; i<=100; i++){
+    console.log(i);
+    if(i%2){
+        console.log(i + "this is even number");
+    }
+    else{
+        console.log(i + "this is odd number")
+    }
+}
