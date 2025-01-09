a34e5b2a929bf7558999e89c0ecbd9780a0ca959
sriramgayaa3 <2211cs010545@mallareddyuniversity.ac.in>
2025-01-09 13:59:23 +0530

    sending message to git

---
+++ function.js
@@ -0,0 +1,33 @@
+//function(){
+//}
+//console.log("Anonymus function");
+//}
+//()
+
+// ! function expression
+//function addToCart(){
+// (function){
+         //console.log("IIFE")};
+// function demo();
+//{
+    //console.log("Hello");
+//}
+//demo();
+
+//let x = () =>console.log("Arrow function");
+//x();
+//let x = ()=>console.log("Arrow function");
+// x();
+
+//let x =-=>consolelog("Arrow function");
+// x;
+
+// let x =a=>console.log(a);
+// x(5);
+
+// let x =(a , b)=>console.log(a+b);
+//x(5,5);
+let x=_=>{console.log("Hi");
+console.log("Hello");
+console.log("Bye");}
+x();
