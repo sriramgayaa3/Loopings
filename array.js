7e6b471a0df65d0f649714e28d47d706e6173b4e
sriramgayaa3 <2211cs010545@mallareddyuniversity.ac.in>
2025-01-20 21:55:12 +0530

    sending files

---
+++ array.js
@@ -0,0 +1,34 @@
+// let user
+
+
+
+
+// console.log(user1);
+// console.log(user2);
+// console.log(user3);
+// console.log(user4);
+// console.log(user5);
+
+let users = ["Ramya" , "SaiSmitha" , "Sai Nikhil" , "Rahul" , "Prabhas" , "NTR"];
+// let x = users.map((user)=>{
+    }
+//     return users; //!["Ramya" , "SaiSmitha", "Sai Nikhil" ,]    
+)
+users.map((users)=>{
+    console.log(user);
+    return user;
+}) ;
+console.log(x); // 
+let usersDetails ={
+    name:"Sam"
+    age:25,
+    city:"Hyd"
+
+}
+console.log(userDetails);
+
+let x = JSON.stringify(userDetails);
+console.log("JSON object "+x);
+
+let y=J
+]
